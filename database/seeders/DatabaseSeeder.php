e4d75fa3b6238cb198a93c67a5d796edafded398
leandrodsg <leandrodsg.pt@gmail.com>
2025-09-17 11:21:11 +0200

    implement roles system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,13 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        \App\Models\User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'role' => 'admin',
+            'password' => bcrypt('admin123'),
+        ]);
+
+        \App\Models\User::factory(3)->create();
     }
 }
