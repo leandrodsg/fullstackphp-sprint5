3a67028f3e7e79efb73a73284b030398c86a951f
leandrodsg <leandrodsg.pt@gmail.com>
2025-10-08 21:31:30 +0200

    Fix Vite config for relative URLs

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,14 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            output: {
+                manualChunks: undefined,
+            }
+        }
+    },
+    base: './',
 });
