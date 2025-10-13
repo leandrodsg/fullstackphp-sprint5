5aa000c0a889920a0d073878e04e4369de8f0e47
leandrodsg <leandrodsg.pt@gmail.com>
2025-10-13 20:30:41 +0200

    Update Passport migrations

--- database/migrations/2025_10_13_140556_create_oauth_auth_codes_table.php
+++ database/migrations/2025_10_13_140556_create_oauth_auth_codes_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('oauth_auth_codes', function (Blueprint $table) {
-            $table->char('id', 80)->primary();
+            $table->string('id', 100)->primary();
             $table->foreignId('user_id')->index();
-            $table->foreignUuid('client_id');
+            $table->uuid('client_id');
             $table->text('scopes')->nullable();
             $table->boolean('revoked');
             $table->dateTime('expires_at')->nullable();
